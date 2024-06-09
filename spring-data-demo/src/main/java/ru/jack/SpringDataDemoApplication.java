79a99fe1d8ad3e8874a5e5ce767b2cdd0b86680c
Yauheni Aviltsau <xopek-007@yandex.ru>
2024-06-09 13:32:09 +0300

    timing 00:25:17 main editing

--- spring-data-demo/src/main/java/ru/jack/SpringDataDemoApplication.java
+++ spring-data-demo/src/main/java/ru/jack/SpringDataDemoApplication.java
@@ -1,13 +1,53 @@
 package ru.jack;
 
+import lombok.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.*;
+import org.springframework.dao.*;
+import org.springframework.data.relational.core.sql.*;
+import org.springframework.jdbc.core.*;
+
+import javax.sql.*;
+import java.sql.*;
 
 @SpringBootApplication
 public class SpringDataDemoApplication {
 
+	@SneakyThrows
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDataDemoApplication.class, args);
-	}
+		ConfigurableApplicationContext context = SpringApplication.run(SpringDataDemoApplication.class, args);
+		JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+
+		//JdbcTemplate
+		
+		jdbcTemplate.execute(new StatementCallback<Integer>() {
+			@Override
+			public Integer doInStatement(Statement stmt) throws SQLException, DataAccessException {
+				return null;
+			}
+
+		});
+
+		DataSource dataSource = context.getBean(DataSource.class);
+
+		try (Connection connection = dataSource.getConnection()) {
+			try (Statement statement = connection.createStatement()) {
+				statement.execute("create table users(id bigint, name varchar(512))");
+			}
+
+			try (Statement statement = connection.createStatement()) {
+				statement.execute("insert into users(id, name) values(1, 'Igor')");
+			}
+
+			try (Statement statement = connection.createStatement()) {
+				ResultSet resultSet = statement.executeQuery("select id, name from users");
+				while (resultSet.next()) {
+					System.out.println(resultSet.getInt("id"));
+					System.out.println(resultSet.getString("name"));
+				}
+			}
+		}
+    }
 
 }
